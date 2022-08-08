ebe3cc5e34f3ca4830469288019fc618eaf54540
kaztic <karthikbhat1225@gmail.com>
2022-08-08 11:03:38 +0530

    FontAndThemes

--- lib/categories_Screen.dart
+++ lib/categories_Screen.dart
@@ -6,12 +6,17 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mealz'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
+          crossAxisSpacing: 15,
+          mainAxisSpacing: 15,
         ),
         children: DUMMY_CATEGORIES
             .map(
--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,28 @@ import 'package:mealz/categories_Screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mealz',
       theme: ThemeData(
-        primaryColor: Colors.blue,
+        primaryColor: Colors.pink,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyText1: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            bodyText2: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            headline6: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+            )),
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.amber),
       ),
       home: CategoriesScreen(),
     );
